f5223997511eb3eabd964716e1f44c93f1fe27b1
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-08-13 03:54:19 -0400

    refactored and shrtened and cleaned up code

--- Bishop.js
+++ Bishop.js
@@ -10,116 +10,81 @@ class Bishop{
         this.allItems = allItems;
         this.ID = ID;
         this.onSquareClicked();
-        // for(let i = 0; i< squares.length;i++){
-        //     squares[i].addEventListener('click',this.onSquareClicked);
-        // };
     }
 
 
     onSquareClicked(){
         
-            console.log("----------------------------------");
-            console.log("whiteMove1 " + this.whiteMove);
-            console.log("blackMove1 " + this.blackMove);
-            console.log("currentPlayer " + this.currentPlayer);
-            if(this.currentPlayer == "White"){
-        
-                if(this.whiteMove == true){
-                    this.currentPosition = this.ID;
-                    this.whiteMove = false;
-                }
-                else{
-                    this.futurePosition = this.ID;
-                    this.whiteMove = true;
-                    this.currentPlayer = "Black";
-                    this.PlaceComputerBishop();              
-                }
+        if(this.currentPlayer == "White"){
+    
+            if(this.whiteMove == true){
+                this.currentPosition = this.ID;
+                this.whiteMove = false;
             }
             else{
-                if(this.blackMove == true){
-                    this.currentPosition = this.ID;
-                    this.blackMove = false;
-                    
-                
+                this.futurePosition = this.ID;
+                this.whiteMove = true; 
+                //this.PlaceComputerPawns();  
+                if(this.PlaceBishop()){
+                    this.currentPlayer = "White";
                 }
                 else{
-                    this.futurePosition = this.ID;
-                    this.blackMove = true;
+                    this.currentPlayer = "Black";
+                }
+                
+                        
+            }
+        }
+        else{
+            if(this.blackMove == true){
+                console.log("firstThis");
+                this.currentPosition = this.ID;
+                this.blackMove = false;
+            }
+            else{
+                console.log("thenThis");
+                this.futurePosition = this.ID;
+                this.blackMove = true;
+                if(this.PlaceBishop()){
+
+                    this.currentPlayer = "Black";
+                }
+                else{
                     this.currentPlayer = "White";
-                    console.log("wereeeeeeeeeeeeeeee");
-                    this.PlaceBishop();
-                    
-                    
-                    
                 }
             }
-
-        
-    
+        }
 
     }
 
 
     PlaceBishop(){
         console.log("fdfbslbk")
-        if(this.attack()){
+        if(this.CheckValidBishopMove()){
             let createImage = document.createElement("img");
-            this.removePonds(this.futurePosition);
-            this.allItems[this.futurePosition] = "BlackBishop.png";
-            document.getElementById(this.futurePosition).appendChild(createImage);
-            createImage.width = "35";
-            createImage.height = "55";
-            createImage.src = this.allItems[this.currentPosition];
-            this.removePonds(this.currentPosition);
+            this.removeBishop(this.futurePosition);
+            if(this.currentPlayer === "Black"){
+                this.allItems[this.futurePosition] = "BlackBishop.png";
+            }
+            else{
+                this.allItems[this.futurePosition] = "WhiteBishop.png";
+            }
             
-        }
-        else if(this.CheckValidBishopMove()){
-            let createImage = document.createElement("img");
-            this.allItems[this.futurePosition] = "BlackBishop.png";
             document.getElementById(this.futurePosition).appendChild(createImage);
             createImage.width = "35";
             createImage.height = "55";
             createImage.src = this.allItems[this.currentPosition];
-            this.removePonds(this.currentPosition);
+            this.removeBishop(this.currentPosition);
         }
         else{
             alert("Move not allowed");
-            this.currentPlayer = "Black";
-            console.log(" this does not work dsss");
-            //blackMove = false;
-        }
-        
-    }
-
-    PlaceComputerBishop(){
-        if(this.attack()){
-            let createImage = document.createElement("img");
-            this.removePonds(this.futurePosition);
-            this.allItems[this.futurePosition] = "WhiteBishop.png";
-            document.getElementById(this.futurePosition).appendChild(createImage);
-            createImage.width = "40";
-            createImage.height = "50";
-            createImage.src = this.allItems[this.currentPosition];
-            this.removePonds(this.currentPosition);
-        }
-        else if(this.CheckValidBishopMove()){
-            let createImage = document.createElement("img");
-            //console.log(value);
-            this.allItems[this.futurePosition] = "WhiteBishop.png";
-            document.getElementById(this.futurePosition).appendChild(createImage);
-            createImage.width = "35";
-            createImage.height = "55";
-            createImage.src = this.allItems[this.currentPosition];
-            this.removePonds(this.currentPosition);
-        }
-        else{
-            console.log(" this does not work dsss");
-            alert("Move not allowed");
-            this.currentPlayer =  "White";
+            return true;
+            
 
         }
         
     }
+
 
     CheckValidBishopMove(){
         let currentPositionRow = parseInt(this.currentPosition);
@@ -129,21 +94,14 @@ class Bishop{
         let futurePositionColumn = Math.floor(futurePositionRow % 10);
         futurePositionRow = Math.floor(futurePositionRow/10);
 
-        console.log("in valid bishop move");
-
-      
-
         let combine = "";
 
         if((currentPositionColumn < futurePositionColumn) && (currentPositionRow < futurePositionRow)){
             let j = currentPositionRow+1;
-            console.log("thissssssddhksfl");
             for(let i = currentPositionColumn + 1; i<futurePositionColumn;i++){
                 combine = "";
                 combine +=j;
                 combine+=i;
-                console.log("combine " + combine);
-                console.log("imageHere " + this.allItems[combine]);
                 if(this.allItems[combine] != "nothing"){
                     console.log("false");
                     return false;
@@ -154,9 +112,7 @@ class Bishop{
         }
         else if(( currentPositionRow < futurePositionRow) && (futurePositionColumn < currentPositionColumn)){
             let j  = currentPositionRow + 1;
-            console.log("dhjkskhdkfkjlfdhklfhkslllllllk333");
             for(let i = currentPositionColumn - 1; i>futurePositionColumn;i--){
-                console.log("dhjkskhdkfkjlfdhklfhkslllllllk");
                 combine = "";
                 combine+=j;
                 combine+=i;
@@ -167,16 +123,12 @@ class Bishop{
             }
         }
         else if((currentPositionColumn < futurePositionColumn) && (currentPositionRow>futurePositionRow)){
-            console.log("thisherei");
             let j  = currentPositionRow - 1;
             for(let i = currentPositionColumn + 1; i<futurePositionColumn;i++){
                 combine = "";
                 combine +=j;
                 combine+=i;
-                console.log("combine " + combine);
-                console.log("imageHere " + this.allItems[combine]);
                 if(this.allItems[combine] != "nothing"){
-                    console.log("false");
                     return false;
                 }
                 j-=1;
@@ -185,7 +137,6 @@ class Bishop{
 
         }
         else{
-            console.log("thisherei398623592");
             let j  = currentPositionRow-1;
             for(let i = currentPositionColumn - 1; i>futurePositionColumn;i--){
                 combine = "";
@@ -200,7 +151,7 @@ class Bishop{
     
          
             
-        if((currentPositionColumn!=futurePositionColumn) && (currentPositionRow!=futurePositionRow) && (this.allItems[this.futurePosition] == "nothing") && (Math.abs(futurePositionColumn-currentPositionColumn) === Math.abs(futurePositionRow-currentPositionRow))){
+        if((currentPositionColumn!=futurePositionColumn) && (currentPositionRow!=futurePositionRow) && (this.currentPlayer !== this.allItems[this.futurePosition].substring(0,5)) && (Math.abs(futurePositionColumn-currentPositionColumn) === Math.abs(futurePositionRow-currentPositionRow))){
             return true;
         }
         else{
@@ -209,46 +160,7 @@ class Bishop{
         
         
     }
-    
-
-    attack(){
-        let numericalCurrentPosition = parseInt(this.currentPosition);
-        let numericalCurrentPositionRemainder  = Math.floor(numericalCurrentPosition % 10);
-        numericalCurrentPosition = Math.floor(numericalCurrentPosition/10);
-        let numericalFutureposition = parseInt(this.futurePosition);
-        let numericalFuturepositionRemainder = Math.floor(numericalFutureposition % 10);
-        numericalFutureposition = Math.floor(numericalFutureposition/10);
-    
-        console.log("in this ")
-    
-    
-        if((numericalCurrentPositionRemainder!=numericalFuturepositionRemainder) && (numericalCurrentPosition!=numericalFutureposition) && (this.allItems[this.futurePosition] != "nothing")){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
-        
-    }
-    
-    // attackPlayer(){
-    //     let numericalCurrentPosition = parseInt(this.currentPosition);
-    //     let numericalCurrentPositionRemainder  = Math.floor(numericalCurrentPosition % 10);
-    //     numericalCurrentPosition = Math.floor(numericalCurrentPosition/10);
-    //     let numericalFutureposition = parseInt(this.futurePosition);
-    //     let numericalFuturepositionRemainder = Math.floor(numericalFutureposition % 10);
-    //     numericalFutureposition = Math.floor(numericalFutureposition/10);
-    
-    
-    //     if((numericalCurrentPositionRemainder!=numericalFuturepositionRemainder) && (numericalCurrentPosition!=numericalFutureposition) && (this.allItems[this.futurePosition] != "nothing")){
-    //         return true;
-    //     }
-    //     else{
-    //         return false;
-    //     }
-    // }
-
+ 
     getDictionary(){
         return this.allItems;
     }
@@ -257,7 +169,7 @@ class Bishop{
         return this.currentPlayer;
     }
 
-    removePonds(elementToRemove){
+    removeBishop(elementToRemove){
         const myNode = document.getElementById(elementToRemove);
         this.allItems[elementToRemove] = "nothing";
       
